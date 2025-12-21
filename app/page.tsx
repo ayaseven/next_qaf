2277eeb454317534b348c6641ca17aeb225174e9
ayaseven <fayamiyagi1@gmail.com>
2025-12-21 04:59:58 -0800

    Added filtering and some user interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import { Container, Filters, Title, TopBar } from '@/components/shared'
+
 export default function Home() {
-  return (
-  <h1>
-   Главная 
-  </h1>
-  );
+	return (
+		<>
+    {/*Зона верхних категорий*/}
+			<Container className='mt-10'>
+				<Title text={'Все меню'} size='lg' className='font-extrabold' />
+			</Container>
+			<TopBar />
+      {/*Зона категории товары и так далее*/}
+			<Container className='mt-10 pb-14'>
+				<div className='flex gap-15'>
+					{/*Фильтрация*/}
+					<div className='w-62.5'>
+						<Filters />
+					</div>
+					{/*Список товаров*/}
+					<div className='flex-1'>
+						<div className='flex flex-col gap-16'>Список товаров</div>
+					</div>
+				</div>
+			</Container>
+		</>
+	)
 }
---
+++ components/shared/checkbox-filter-group.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import React from 'react'
+import { Input } from '../ui'
+import { FilterCheckbox, FilterCheckboxProps } from './filter-checkbox'
+
+type Item = FilterCheckboxProps
+interface Props {
+	title: string
+	items: Item[]
+	defaultItems: Item[]
+	limit: number
+	searchInputPlaceholder?: string
+	onChange?: (values: string[]) => void
+	defaultValue?: string[]
+	className?: string
+}
+
+export const CheckboxFilterGroup: React.FC<Props> = ({
+	title,
+	items,
+	defaultItems,
+	limit,
+	searchInputPlaceholder,
+	onChange,
+	defaultValue,
+	className,
+}) => {
+	const [showAll, setShowAll] = React.useState(false)
+	const list = showAll ? items : defaultItems?.slice(0, limit)
+	return (
+		<div className={className}>
+			<p className='font-bold mb-3'>{title}</p>
+			{/*Поиск*/}
+			{showAll && (
+				<div className='mb-5'>
+					<Input
+						placeholder={searchInputPlaceholder}
+						className='bg-gray-50 border-none'
+					/>
+				</div>
+			)}
+			<div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'>
+				{list.map((item, i) => (
+					<FilterCheckbox
+						key={i}
+						value={item.value}
+						text={item.text}
+						endAdornment={item.endAdornment}
+						checked={false}
+						onCheckedChange={ids => {
+							console.log(ids)
+						}}
+					/>
+				))}
+			</div>
+				{items.length > limit && (
+					<div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+						<button
+							onClick={() => setShowAll(!showAll)}
+							className='text-primary mt-3'
+						>
+							{showAll ? 'Скрыть' : '+ Показать все'}
+						</button>
+					</div>
+				)}
+		</div>
+	)
+}
