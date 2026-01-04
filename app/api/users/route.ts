61fa155b005295200b537834360054743a742c92
ayaseven <fayamiyagi1@gmail.com>
2026-01-03 22:45:18 -0800

    fix

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,14 @@
 import { prisma } from '@/prisma/prisma-client'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
 	const users = await prisma.user.findMany()
 	return NextResponse.json(users)
+}
+export async function POST(req: NextRequest) {
+	const body = await req.json()
+	const user = await prisma.user.create({
+		data: body,
+	})
+	return NextResponse.json(user)
 }
