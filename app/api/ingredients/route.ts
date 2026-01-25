de4ed7eab1e80e6da6f6435b22b135e6a9e1c08b
ayaseven <fayamiyagi1@gmail.com>
2026-01-25 08:01:10 -0800

    I've completed the basic backend and improved the product search. I'm moving to the frontend.

--- app/api/ingredients/route.ts
+++ app/api/ingredients/route.ts
@@ -1,14 +1,10 @@
 import { prisma } from '@/prisma/prisma-client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
-	const users = await prisma.user.findMany()
-	return NextResponse.json(users)
-}
 export async function POST(req: NextRequest) {
 	const body = await req.json()
-	const users = await prisma.user.create({
+	const ingredients = await prisma.ingredient.create({
 		data: body,
 	})
-	return NextResponse.json(users)
+	return NextResponse.json(ingredients)
 }
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,14 +1,14 @@
 import { prisma } from '@/prisma/prisma-client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
-	const users = await prisma.user.findMany()
-	return NextResponse.json(users)
-}
 export async function POST(req: NextRequest) {
 	const body = await req.json()
-	const users = await prisma.user.create({
+	const products = await prisma.product.create({
 		data: body,
 	})
-	return NextResponse.json(users)
+	return NextResponse.json(products)
+}
+export async function GET() {
+	const products = await prisma.product.findMany()
+	return NextResponse.json(products)
 }
